b55763e1cfefc9d88f950d6e45c790808a90973e
Valentin <valentin.lucet@gmail.com>
2021-01-20 12:34:56 -0500

    add arguments to spaspm (for later)

--- R/spaspm.R
+++ R/spaspm.R
@@ -1,6 +1,12 @@
 # Model Object Constructor
 #' @export
-spaspm <- function(name="SPSPM Model", data, boundaries){
+spaspm <- function(name="SPSPM Model",
+                   data,
+                   boundaries,
+                   discretize = TRUE,
+                   discretization_method = "tesselate_voronoi",
+                   fit_gam = TRUE,
+                   ...){
 
   # Check name to be character and data to be a data.frame/tibble
   checkmate::assert_class(name, "character")
