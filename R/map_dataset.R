0e8966d9e05abb6fc5b692e2154bf982baefe246
Valentin <valentin.lucet@gmail.com>
2021-02-06 11:48:08 -0500

    progress, doc + comments for later

--- R/map_dataset.R
+++ R/map_dataset.R
@@ -1,10 +1,10 @@
 #' Map dataset onto a discretized SPASPM object
 #'
-#' Template description
+#' TODO
 #'
 #' @export
 setGeneric(name = "map_dataset",
-           def = function(spaspm_object, ...){
+           def = function(spaspm_object, dataset, ...){
              standardGeneric("map_dataset")
            }
 )
@@ -12,15 +12,47 @@ setGeneric(name = "map_dataset",
 # Methods -----------------------------------------------------------------
 
 #' @export
-#' @describeIn ... TODO
+#' @describeIn map_dataset TODO
 setMethod(f = "map_dataset",
-          signature(spaspm_object = "spaspm_discrete"),
-          function(spaspm_object, ...){
+          signature(spaspm_object = "spaspm_discrete",
+                    dataset = "data.frame"),
+          function(spaspm_object, dataset, ...){
+
+            # call as_spaspm_data
+            # call next method
+
           }
 )
 
 #' @export
-#' @describeIn ... TODO
+#' @describeIn map_dataset TODO
+setMethod(f = "map_dataset",
+          signature(spaspm_object = "spaspm_discrete",
+                    dataset = "spaspm_data"),
+          function(spaspm_object, dataset, ...){
+
+            # append to list of mapped_datasets
+            # return new spaspm_discretized
+
+          }
+)
+
+#' @export
+#' @describeIn map_dataset TODO
+setMethod(f = "map_dataset",
+          signature(spaspm_object = "spaspm_discrete",
+                    dataset = "list"),
+          function(spaspm_object, dataset, ...){
+
+            # identify if list of data.frames OR
+            # list of spaspm_data,
+            # lapply acoordingly
+
+          }
+)
+
+#' @export
+#' @describeIn map_dataset TODO
 setMethod(f = "map_dataset",
           signature(spaspm_object = "spaspm"),
           function(spaspm_object, ...){
