75b07852e3e23f7345a67cb9a3beae173dd2d961
Valentin <valentin.lucet@gmail.com>
2021-02-09 16:11:26 -0500

    all tests now pass

--- tests/testthat/test-accessors.R
+++ tests/testthat/test-accessors.R
@@ -1,30 +1,12 @@
-# Class construction
-
-test_that("spaspm object is created correctly", {
-  expect_class({spaspm_base <- new("spaspm",
-                                   name="Model test",
-                                   data=borealis,
-                                   boundaries=sfa)}, "spaspm")
-})
-
-test_that("spaspm_discrete object is created correctly", {
-  expect_class({spaspm_base <- new("spaspm_discrete",
-                                   name = spm_name(spaspm_base),
-                                   data = spm_data(spaspm_base),
-                                   boundaries = spm_boundaries(spaspm_base),
-                                   data_spatial = borealis_spatial,
-                                   method = the_method,
-                                   patches = the_patches,
-                                   points = the_points)}, "spaspm_discrete")
-})
-
 # Test accessors
 test_that("accessors work on spaspm", {
 
   expect_class({spm_name(spaspm_base)}, "character")
-  expect_class({spm_data(spaspm_base)}, "data.frame")
+  expect_class({spm_base_dataset(spaspm_base)}, "spaspm_data")
   expect_class({spm_boundaries(spaspm_base)}, "sf")
 
+  expect_message({spm_data(spaspm_base)},
+                 "Use `spm_datasets` or `spm_base_dataset` to access the datasets of a spaspm object")
   expect_message({spm_discret_method(spaspm_base)},
                  "Model object 'Model test' is not a discrete model")
   expect_message({spm_patches(spaspm_base)},
@@ -36,7 +18,7 @@ test_that("accessors work on spaspm", {
 test_that("accessors work on spaspm_discrete", {
 
   expect_class({spm_name(spaspm_discrete)}, "character")
-  expect_class({spm_data(spaspm_discrete)}, "list")
+  expect_class({spm_base_dataset(spaspm_discrete)}, "list")
   expect_class({spm_boundaries(spaspm_discrete)}, "sf")
 
   expect_class({spm_discret_method(spaspm_discrete)}, "discretization_method")
