7825d683b92a8f65abaaf68b3853e40d1a919bae
Valentin <valentin.lucet@gmail.com>
2021-02-09 16:02:58 -0500

    udpate tests to use the internal datasets

--- tests/testthat/helper-tests.R
+++ tests/testthat/helper-tests.R
@@ -3,33 +3,37 @@ library(dplyr)
 library(sf)
 
 # Objects used for tests
-borealis <- as.data.frame(readRDS("../../data/borealis_full_summary.rds")) %>%
-  dplyr::filter(season!="summer") %>%
-  dplyr::arrange(trip)
-predator <- as.data.frame(readRDS("../../data/predator_full_summary.rds"))
+borealis_simulated <- spaspm:::borealis_simulated
+predator_simulated <- spaspm:::predator_simulated
 
-borealis_spatial <- st_as_sf(borealis, coords = c('lon_dec','lat_dec'))
-predator_spatial <- st_as_sf(predator, coords = c('lon_dec','lat_dec'))
+borealis_spatial <- st_as_sf(borealis_simulated, coords = c('lon_dec','lat_dec'))
+predator_spatial <- st_as_sf(predator_simulated, coords = c('lon_dec','lat_dec'))
 
-sfa <- st_read("../../data/sfa/sfa_boundaries.shp", quiet=TRUE)
-the_patches <- st_read("../../data/patches/patches.shp", quiet=TRUE)
-the_points <- st_read("../../data/patches/patches.shp", quiet=TRUE)
+sfa_boundaries <- spaspm:::sfa_boundaries
+borealis_patches <- spaspm:::borealis_patches
+borealis_points <- spaspm:::borealis_points
 
 # Base objects
+spaspm_data <- new("spaspm_data",
+                   name = "Biomass",
+                   data = borealis_simulated,
+                   uniqueID = "uniqueID",
+                   coords = c('lon_dec','lat_dec'),
+                   representation = "Simple feature collection")
+
 spaspm_base <- new("spaspm",
                    name="Model test",
-                   data=borealis,
-                   boundaries=sfa)
+                   data=spaspm_data,
+                   boundaries=sfa_boundaries)
 
-the_method <- new("discretization_method",
-                  name ="voronoi_method",
-                  method = tesselate_voronoi)
+discret_method <- new("discretization_method",
+                      name ="voronoi_method",
+                      method = tesselate_voronoi)
 
 spaspm_discrete <- new("spaspm_discrete",
                        name = spm_name(spaspm_base),
-                       data = spm_data(spaspm_base),
+                       data = spm_base_dataset(spaspm_base),
                        boundaries = spm_boundaries(spaspm_base),
-                       data_spatial = borealis_spatial,
-                       method = the_method,
-                       patches = the_patches,
-                       points = the_points)
+                       method = discret_method,
+                       patches = borealis_patches,
+                       points = borealis_points)
