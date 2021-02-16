eb3311b990d730b411ff101c83cd86362066c84b
Valentin <valentin.lucet@gmail.com>
2021-02-16 16:17:48 -0500

    fix as_spaspm_smooth to support character input (simplify as well)

--- R/as_spaspm_smooth.R
+++ R/as_spaspm_smooth.R
@@ -3,10 +3,14 @@
 #' This function allows to cast different inputs into an object of class
 #' [`spaspm_smooth`][spaspm_smooth-class].
 #'
-#' @param smooth **\[mgcv xxx.smooth.spec object\]** A smooth object from [mgcv].
+#' @param smooth **\[mgcv xxx.smooth.spec object OR character\]** A smooth
+#'     object from [mgcv] or a character value from the suppported default types. See
+#'     [spm_smooth_types()][spm_smooth_types].
 #' @param dataset_name **\[character\]** The name of the dataset on which to
 #'     map the smooth. See also [spaspm_smooth][spaspm_smooth-class].
-#' @param type **\[character\]**
+#' @param dimension **\[character\]** The smoothing dimension the smooth is
+#'     applied to: one if "space", "time", "space_time".
+#' @param ... Further arguments passed on to build the smooth `TODO`.
 #'
 #' @return
 #' An object of class [`spaspm_smooth`][spaspm_smooth-class].
@@ -15,7 +19,14 @@
 #'
 #' @export
 setGeneric(name = "as_spaspm_smooth",
-           def = function(smooth, dataset_name, type, ...){
+           def = function(smooth, dataset_name, dimension, ...){
+
+             if(!checkmate::test_choice(dimension, spm_dimensions())){
+               stop(paste0("Dimension must be one of: ",
+                           paste0(spm_dimensions(),
+                                  collapse =  ", " )))
+             }
+
              standardGeneric("as_spaspm_smooth")
            }
 )
@@ -37,53 +48,58 @@ setMethod(f = "as_spaspm_smooth",
 #' @export
 setMethod(f = "as_spaspm_smooth",
           signature(smooth = "ANY",
-                    dataset_name = "missing"),
-          function(smooth, dataset_name, type, ...){
-            cli::cli_alert_danger("Required argument `dataset_name` missing or not specified with `dataset_name=`")
-
+                    dataset_name = "missing",
+                    dimension = "ANY"),
+          function(smooth, dataset_name, dimension, ...){
+            cli::cli_alert_danger("Required argument `dataset_name` missing or not specified with `dataset_name =`")
           }
 )
 
-# Assess mgcv smooth and cast it
+# If character, check against values and proceed to next method
+#' @describeIn as_spaspm_smooth TODO
+#' @export
+setMethod(f = "as_spaspm_smooth",
+          signature(smooth = "character",
+                    dataset_name = "character",
+                    dimension = "character"),
+          function(smooth, dataset_name, dimension, ...){
+
+            if(!checkmate::test_choice(smooth, spm_smooth_types())){
+              paste0("Smooth type must be one of: ", paste0(spm_smooth_types(),
+                                                            collapse =  ", " ))
+            }
+
+            # TODO collect args from ... and check for time/space here for ICAR
+
+            smooth <- dispatch_smooth_type(smooth, dimension, ...)
+
+            smooth_object <-  new("spaspm_smooth",
+                                  representation = get_base_smooth_type(smooth),
+                                  dataset_name = dataset_name,
+                                  dimension = dimension,
+                                  smooth = smooth)
+
+            return(smooth_object)
+          }
+)
+
+# If not a character value, assess if mgcv smooth and cast it
 #' @describeIn as_spaspm_smooth TODO
 #' @export
 setMethod(f = "as_spaspm_smooth",
           signature(smooth = "ANY",
                     dataset_name = "character",
-                    type = "missing"),
-          function(smooth, dataset_name, type, ...){
+                    dimension = "character"),
+          function(smooth, dataset_name, dimension, ...){
             if (is_smooth_spec(smooth)){
               the_smooth <- new("spaspm_smooth",
                                 representation = get_base_smooth_type(smooth),
                                 dataset_name = dataset_name,
+                                dimension = dimension,
                                 smooth = smooth)
               return(the_smooth)
             } else{
-              cli::cli_alert_danger("Argument `smooth` must be of type `xxx.smooth.spec`")
+              cli::cli_alert_danger("Argument `smooth` must be of type `xxx.smooth.spec` or a character value")
             }
-          }
-)
-
-# TODO doesnt work as expected
-#' @describeIn as_spaspm_smooth TODO
-#' @export
-setMethod(f = "as_spaspm_smooth",
-          signature(smooth = "ANY",
-                    dataset_name = "character",
-                    type = "ANY"),
-          function(smooth, dataset_name, type, ...){
-            cli::cli_alert_danger("Argument `type` cannot be specified when `smooth` is specified")
-          }
-)
-
-# If type (ICAR, etc...)
-#' @describeIn as_spaspm_smooth TODO
-#' @export
-setMethod(f = "as_spaspm_smooth",
-          signature(smooth = "missing",
-                    dataset_name = "character",
-                    type = "character"),
-          function(smooth, dataset_name, type, ...){
-            # Check if types
           }
 )
