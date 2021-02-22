b730fff208ff018b14541210e142fb02d7c392a1
Valentin <valentin.lucet@gmail.com>
2021-02-22 12:07:41 -0500

    add base code developped last week for map_formula

--- R/map_formula.R
+++ R/map_formula.R
@@ -32,6 +32,76 @@ setGeneric(name = "map_formula",
 setMethod(f = "map_formula",
           signature(spaspm_object = "spaspm"),
           function(spaspm_object, ...){
-            message_not_discrete()
+            message_not_discrete(spaspm_object)
+          }
+)
+
+#' @export
+#' @describeIn map_formula TODO
+setMethod(f = "map_formula",
+          signature(spaspm_object = "spaspm_discrete",
+                    dataset = "missing"),
+          function(spaspm_object, dataset, formula, ... ){
+            cli::cli_alert_danger(" Argument 'dataset' missing with no default")
+          }
+)
+
+#' @export
+#' @describeIn map_formula TODO
+setMethod(f = "map_formula",
+          signature(spaspm_object = "spaspm_discrete",
+                    formula = "missing"),
+          function(spaspm_object, dataset, formula, ... ){
+            cli::cli_alert_danger(" Argument 'formula' missing with no default")
+          }
+)
+
+#' @export
+#' @describeIn map_formula TODO
+setMethod(f = "map_formula",
+          signature(spaspm_object = "spaspm_discrete",
+                    dataset = "character",
+                    formula = "formula"),
+          function(spaspm_object, dataset, formula, ... ){
+
+            # retrieve terms, response, and term labels
+            formula_terms <- terms(formula)
+            response <- all.vars(formula)[1]
+            terms_labels <- attr(formula_terms, "term.labels")
+
+            # Find the special calls to edit and evaluate
+            is_special <- sapply(terms_labels, grepl, pattern="smooth_", fixed=TRUE)
+            smooth_terms_labels <- terms_labels[is_special]
+            other_terms <- terms_labels[!is_special]
+
+            # Reconstruct base formula
+            base_formula <- (paste(response, "~", paste(other_terms, collapse = " + ")
+                                   , collapse = " "))
+
+            # Capture calls and modify them
+            smooth_calls <- lapply(smooth_terms_labels, str2lang)
+
+            smooth_calls_modified <-
+              lapply(smooth_calls, modify_call,
+                     args = list(spaspm_object = substitute(spaspm_object),
+                                 dataset = substitute(dataset)))
+
+            # Evaluate the calls to get the quoted smooths
+            evaluated <- lapply(smooth_calls_modified, eval)
+            evaluated_string <- sapply(evaluated, deparse)
+
+            # Paste them into formula
+            final_formula <- paste0(base_formula, " + ",
+                                    paste(evaluated_string, collapse = " + "))
+
+            # Cast as formula
+            # TODO unsure if it's worth casting here
+            # final_formula_casted <- as.formula(final_formula)
+
+            # Returns an updated spaspm object WITH mapped formula
+            # TODO add the formula to dataset (we know which from argument)
+
+            updated_spaspm_object <- spaspm_object
+            return(updated_spaspm_object)
           }
 )
