19111ca9b11adf41c51cc9e23037c54c22742ad2
Valentin <valentin.lucet@gmail.com>
2021-02-22 12:17:59 -0500

    define methods for smooth_time as a test

--- R/smooths.R
+++ R/smooths.R
@@ -4,11 +4,15 @@
 #' `smooth_space()`, `smooth_space_time()`.
 #'
 #' @param type **\[character\]** Type of smooths.
+#' @inheritParams map_formula
 #'
 #' @rdname smooths
 #' @export
 setGeneric(name = "smooth_time",
-           def = function(type){
+           def = function(type = "ICAR",
+                          dataset,
+                          spaspm_object,
+                          ...){
              standardGeneric("smooth_time")
            }
 )
@@ -16,7 +20,10 @@ setGeneric(name = "smooth_time",
 #' @export
 #' @describeIn smooths TODO
 setGeneric(name = "smooth_space",
-           def = function(type){
+           def = function(type = "ICAR",
+                          dataset,
+                          spaspm_object,
+                          ...){
              standardGeneric("smooth_space")
            }
 )
@@ -24,17 +31,26 @@ setGeneric(name = "smooth_space",
 #' @export
 #' @describeIn smooths TODO
 setGeneric(name = "smooth_space_time",
-           def = function(type){
+           def = function(type = "ICAR",
+                          dataset,
+                          spaspm_object,
+                          ...){
              standardGeneric("smooth_space_time")
            }
 )
 
-# # Methods -----------------------------------------------------------------
-#
-# #' @export
-# #' @describeIn ... TODO
-# setMethod(f = "",
-#           signature(spaspm_object = ""),
-#           function(spaspm_object, ...){
-#           }
-# )
+# Methods -----------------------------------------------------------------
+
+#' @export
+#' @describeIn smooths TODO
+setMethod(f = "smooth_time",
+          signature(type = "ANY",
+                    dataset = "character",
+                    spaspm_object = "spaspm_discrete"),
+          function(type, dataset, spaspm_object, ...){
+
+
+
+
+          }
+)
