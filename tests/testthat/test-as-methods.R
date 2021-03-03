ee3874f5341f7ad3bb0dcf14c66ba78ebade3f4e
Valentin <valentin.lucet@gmail.com>
2021-03-03 10:36:28 -0500

    add tests

--- tests/testthat/test-as-methods.R
+++ tests/testthat/test-as-methods.R
@@ -10,3 +10,40 @@ test_that("Discret method is casted correctly", {
                  "Method 'method_not_supported' is not part of the supported methods.")
 
 })
+
+test_that("Spaspm data is casted correctly", {
+
+  # Test the 3 generic cases
+  expect_error({
+    as_spaspm_data(data = borealis_simulated,
+                   time_col = "year_f",
+                   coords = c('lon_dec','lat_dec'),
+                   name = "Biomass",
+                   uniqueID = "Bad column")
+  }, "`uniqueID` must be a column of `data`")
+
+  borealis_simulated_NU <- borealis_simulated
+  borealis_simulated_NU$new_col <- "Non_unique"
+  expect_error({
+    as_spaspm_data(data = borealis_simulated_NU,
+                   time_col = "year_f",
+                   coords = c('lon_dec','lat_dec'),
+                   name = "Biomass",
+                   uniqueID = "new_col")
+  }, "`uniqueID` must be unique for each row of `data`")
+
+  expect_error({
+    as_spaspm_data(data = borealis_simulated,
+                   time_col = "Bad column",
+                   coords = c('lon_dec','lat_dec'),
+                   name = "Biomass",
+                   uniqueID = "uniqueID")
+  }, "`time_col` must be a column of `data`")
+
+  # If data matrix is df, coords must be provided
+
+  # Coords must be columns of data
+
+  # When works fine
+
+})
