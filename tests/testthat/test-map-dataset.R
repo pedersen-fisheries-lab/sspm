fa33dab67748d8b3fa9f762440da965f18526cd7
Valentin <valentin.lucet@gmail.com>
2021-03-08 12:04:59 -0500

    cover different case

--- tests/testthat/test-map-dataset.R
+++ tests/testthat/test-map-dataset.R
@@ -7,8 +7,13 @@ test_that("Mapping of dataset works as expected", {
 
   expect_length(spm_mapped_datasets(sspm_discrete), 0)
   mapped <- sspm_discrete %>%
-    map_dataset(predator_spatial, name = "pred_data", time_col = "year",
+    map_dataset(predator_sim, name = "pred_data", time_col = "year",
                 uniqueID = "uniqueID", coords = c("lon_dec", "lat_dec"))
   expect_length(spm_mapped_datasets(mapped), 1)
+
+  mapped_from_df <- sspm_discrete %>%
+    map_dataset(predator_simulated, name = "pred_data", time_col = "year",
+                uniqueID = "uniqueID", coords = c("lon_dec", "lat_dec"))
+  expect_length(spm_mapped_datasets(mapped_from_df), 1)
 
 })
