25c4d57468a650d4dd7ae4022fd9b1fdce3fadad
Valentin <valentin.lucet@gmail.com>
2021-03-09 14:15:25 -0500

    add cases

--- tests/testthat/test-show.R
+++ tests/testthat/test-show.R
@@ -7,5 +7,7 @@ test_that("Shpw methods print out fine", {
   expect_output(show(sspm_formula))
   expect_output(show(discret_method))
   expect_output(show(sspm_discrete))
+  expect_output(show(sspm_discrete_mapped))
+  expect_output(show(sspm_discrete_mapped_forms))
 
 })
