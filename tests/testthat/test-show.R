6f516b67eb342976b0461dfd2fad64a9af311499
Valentin <valentin.lucet@gmail.com>
2021-03-09 14:12:15 -0500

    add dummy tests for show

--- tests/testthat/test-show.R
+++ tests/testthat/test-show.R
@@ -1,1 +1,11 @@
 # Test the show methods
+
+test_that("Shpw methods print out fine", {
+
+  expect_output(show(sspm_base))
+  expect_output(show(sspm_data))
+  expect_output(show(sspm_formula))
+  expect_output(show(discret_method))
+  expect_output(show(sspm_discrete))
+
+})
