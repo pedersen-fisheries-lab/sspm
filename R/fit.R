b43522735912fb0929bdbc4c692fa777384a2b51
Valentin <valentin.lucet@gmail.com>
2021-03-17 15:06:55 -0400

    remove browser statement

--- R/fit.R
+++ R/fit.R
@@ -37,8 +37,6 @@ setMethod(f = "fit_smooths",
           signature(sspm_object = "sspm_discrete"),
           function(sspm_object, family, drop.unused.levels, method, ...){
 
-            browser()
-
             # Get all datasets
             datasets <- spm_datasets(sspm_object)
 
