ee523aad29e318119750af98da9c271ec9b8d411
Valentin <valentin.lucet@gmail.com>
2021-03-17 12:55:39 -0400

    remove ability to re-discretize for now

--- R/spm_discretize.R
+++ R/spm_discretize.R
@@ -13,9 +13,6 @@
 #'    [discretization_method][discretization_method-class].
 #' @param ... **\[named list\]** Further arguments to be passed onto the function
 #'    used in the `discretization_method`.
-#' @param force **\[boolean\]** Only used when calling `spm_discretize` onto
-#'     an object of class [sspm_discrete][sspm-class]: whether you wish
-#'     to force the re-discretization of that model.
 #'
 #' @return
 #' An object of class [sspm_discrete][sspm-class] (the updated
@@ -168,58 +165,22 @@ setMethod(f = "spm_discretize",
           }
 )
 
-# RE-discretization, to review!
-
+# RE-discretization not allowed for now
 #' @describeIn spm_discretize TODO
 #' @export
 setMethod(f = "spm_discretize",
           signature(sspm_object = "sspm_discrete",
-                    with_dataset = "character",
-                    discretization_method = "discretization_method"),
-          function(sspm_object, with_dataset, discretization_method, force = FALSE, ...){
-
-            checkmate::assert_logical(force)
-
-            if (!force){
+                    with_dataset = "ANY",
+                    discretization_method = "ANY"),
+          function(sspm_object, with_dataset, discretization_method, ...){
 
               cli::cli_alert_danger(paste0(" Model '", spm_name(sspm_object),
                                            "' is already discretized"))
-              cli::cli_alert_info(" Use 'force = TRUE' to discretize again. but mapped datasets/smooths will be lost!")
-
-            } else{
-
-              cli::cli_alert_info(paste0(" Re-discretizing model '",
-                                         spm_name(sspm_object), "'"))
-
-              # Strip old geometry
-              the_data <- spm_data(spm_base_dataset(sspm_object))
-              st_geometry(the_data) <- NULL
-
-              # Remove the columns that are in common
-              names_to_remove <- names(spm_boundaries(sspm_object))
-              names_to_remove <- names_to_remove[names_to_remove %in%
-                                                   names(the_data)]
-
-              the_data <- the_data %>%
-                dplyr::select(-c(dplyr::all_of(names_to_remove), "patch_id", "area_km2"))
-
-              new_object <- sspm(model_name = spm_name(sspm_object),
-                                 name = spm_name(spm_base_dataset(sspm_object)),
-                                 data = the_data,
-                                 time_column = spm_time_column(sspm_object),
-                                 coords = spm_coords_col(sspm_object),
-                                 uniqueID = spm_unique_ID(sspm_object),
-                                 boundaries = spm_boundaries(sspm_object))
-
-              spm_discretize(new_object,
-                             discretization_method = discretization_method,
-                             ...)
-            }
           }
 )
 
 
-# Join helper
+## IMPORTANT Join helper
 join_datasets <- function(sspm_data, sspm_object){
 
   checkmate::assert_class(sspm_data, "sspm_data")
