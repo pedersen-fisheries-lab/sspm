1c9b5060ae959e631452f743215045fb24ddf18f
Valentin <valentin.lucet@gmail.com>
2021-03-23 16:30:05 -0400

    export new function

---
+++ R/spm_split.R
@@ -0,0 +1,61 @@
+#' Split data in test and train sets
+#'
+#' Split data before fitting spm.
+#'
+#' @export
+setGeneric(name = "spm_split",
+           def = function(sspm_object,
+                          dataset,
+                          predicate){
+             standardGeneric("spm_split")
+           }
+)
+
+# Methods -----------------------------------------------------------------
+
+#' @export
+#' @rdname spm_split
+setMethod(f = "spm_split",
+          signature(sspm_object = "sspm"),
+          function(sspm_object){
+            message_not_discrete(sspm_object)
+          }
+)
+
+#' @export
+#' @rdname spm_split
+setMethod(f = "spm_split",
+          signature(sspm_object = "sspm_discrete",
+                    dataset = "missing"),
+          function(sspm_object, dataset, predicate){
+            stop("Argument 'dataset' missing")
+          }
+)
+
+#' @export
+#' @rdname spm_split
+setMethod(f = "spm_split",
+          signature(sspm_object = "sspm_discrete",
+                    dataset = "character"),
+          function(sspm_object, dataset, predicate){
+
+            # Check correct dataset name
+            all_datasets <- spm_datasets(sspm_object)
+            all_datasets_names <- sapply(all_datasets, spm_name)
+
+            if(!(dataset %in% all_datasets_names)){
+              stop("Wrong dataset name.")
+            }
+
+            # Check dataset is smoothed
+            if(!all_datasets[[dataset]]@is_smoothed){
+              stop("Dataset not smooth")
+            }
+
+            the_data <- spm_data(all_datasets[[dataset]])
+
+            browser()
+
+          }
+)
+
