4cc55ede5bff95c59a17fd3b6786b8f4d6fc1435
Valentin <valentin.lucet@gmail.com>
2021-05-17 11:04:40 -0400

    start work on predict

--- R/predict.R
+++ R/predict.R
@@ -21,5 +21,25 @@ setGeneric(name = "spm_predict",
 setMethod(f = "spm_predict",
           signature(sspm_object = "sspm_discrete"),
           function(sspm_object, new_data, ...){
+
+            smoothed_data <- spm_smoothed_data(sspm_object)
+
+            if(is.null(new_data)){
+
+              new_data <- spm_data(smoothed_data)
+
+            }
+
+            fits <- spm_smoothed_fit(smoothed_data)
+            fits_length <- length(fits)
+
+            fit_tmp <- list()
+
+            for (fit_id in seq_len(length.out = fits_length)){
+
+
+
+            }
+
           }
 )
