230d8d2d4d597433af12e677b5be6fa54d88fffe
Valentin <valentin.lucet@gmail.com>
2021-06-04 10:16:26 -0400

    continue work on new spm_smooth

--- R/spm_smooth.R
+++ R/spm_smooth.R
@@ -5,14 +5,14 @@
 #'
 #' @param sspm_object **\[sspm_data\]** An object of class
 #'     [sspm_data][sspm_data-class].
-#' @param boundary **\[sspm_boundary\]** An object of class
-#'     [sspm_boundary][sspm_boundary-class].
 #' @param formula **\[formula\]** A formula definition of the form
 #'     response ~ smoothing_terms + ...
+#' @param boundaries **\[sspm_boundary\]** An object of class
+#'     [sspm_discrete_boundary][sspm_boundary-class].
 #' @param keep_fit **\[logical\]** Whether or not to keep the fitted values and
-#'   model (default to TRUE, set to FALSE to reduce memory footprint).
+#'     model (default to TRUE, set to FALSE to reduce memory footprint).
 #' @param predict **\[logical\]** Whether or not to generate the smoothed
-#'   predictions (necessary to fit the final SPM model, default to TRUE).
+#'     predictions (necessary to fit the final SPM model, default to TRUE).
 #' @inheritDotParams mgcv::bam
 #'
 #' @details
@@ -26,6 +26,7 @@
 setGeneric(name = "spm_smooth",
            def = function(sspm_object,
                           formula,
+                          boundaries,
                           keep_fit = FALSE,
                           predict = TRUE,
                           ...){
@@ -38,24 +39,60 @@ setGeneric(name = "spm_smooth",
 #' @export
 #' @rdname map_formula
 setMethod(f = "spm_smooth",
-          signature(sspm_object = "sspm_data",
-                    formula = "missing"),
-          function(sspm_object, formula, ...){
+          signature(sspm_object = "ANY",
+                    formula = "missing",
+                    boundaries = "ANY"),
+          function(sspm_object, formula, boundaries, keep_fit, predict, ...){
             cli::cli_alert_danger(" Argument 'formula' missing with no default")
+          }
+)
+
+#' @export
+#' @rdname map_formula
+setMethod(f = "spm_smooth",
+          signature(sspm_object = "ANY",
+                    formula = "ANY",
+                    boundaries = "missing"),
+          function(sspm_object, formula, boundaries, keep_fit, predict, ...){
+            cli::cli_alert_danger(" Argument 'boundaries' missing with no default")
+          }
+)
+
+#' @export
+#' @rdname map_formula
+setMethod(f = "spm_smooth",
+          signature(sspm_object = "ANY",
+                    formula = "ANY",
+                    boundaries = "sspm_boundary"),
+          function(sspm_object, formula, boundaries, keep_fit, predict, ...){
+            cli::cli_alert_danger(" Argument 'boundaries' must have been discretized")
           }
 )
 
 #' @export
 #' @rdname spm_smooth
 setMethod(f = "spm_smooth",
-          signature(sspm_object = "sspm_data"),
-          function(sspm_object, formula, keep_fit, predict,...){
+          signature(sspm_object = "sspm_data",
+                    formula = "formula",
+                    boundaries = "sspm_discrete_boundary"),
+          function(sspm_object, formula, boundaries, keep_fit, predict, ...){
 
-            # 1. Map bound
+            # 1. Map boundary data
+            if(!is_mapped(sspm_object)){
+              sspm_object_joined <- join_datasets(sspm_object, boundaries)
+            } else {
+              sspm_object_joined <- sspm_object
+            }
 
-            # 1. call map_formula (needs to be modified)
+            # 1. call map_formula
+            sspm_object_joined <- sspm_object_joined %>%
+              map_formula(boundaries = boundaries, formula = formula)
 
             # 2. call fit with ... arguments
+            sspm_object_fitted <- sspm_object_joined %>%
+              fit_smooths(boundaries = boundaries,
+                          keep_fit = keep_fit, predict = predict, ...)
 
+            return(sspm_object_fitted)
           }
 )
