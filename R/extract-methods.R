4e74b6585d5349ad10bf52a85f1a8ec45203bce4
Valentin <valentin.lucet@gmail.com>
2021-06-14 10:48:06 -0400

    modify extract methods for convenience

--- R/extract-methods.R
+++ R/extract-methods.R
@@ -4,20 +4,28 @@
 setMethod("$",
           "sspm_boundary",
           function(x, name) {
-            x@boundaries[[name]]
+            x@boundaries %>%
+              dplyr::select(c(name, spm_boundary_colum(x), "geometry"))
           }
 )
 
 setMethod("$",
           "sspm_discrete_boundary",
           function(x, name) {
-            x@boundaries[[name]]
+            x@boundaries %>%
+              dplyr::select(c(name, spm_boundary_colum(x), "geometry"))
           }
 )
 
 setMethod("$",
           "sspm_data",
           function(x, name) {
-            x@data[[name]]
+            if (is.null(x@smoothed_data)){
+              x@data %>%
+                dplyr::select(c(name, spm_time_column(x), "geometry"))
+            }else{
+              x@smoothed_data %>%
+                dplyr::select(c(name, spm_time_column(x), "geometry"))
+            }
           }
 )
