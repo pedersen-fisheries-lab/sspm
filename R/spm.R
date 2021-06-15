b67cd5beafc8a06da61f823b1c3f11068f000b57
Valentin <valentin.lucet@gmail.com>
2021-06-15 15:52:22 -0400

    continue improvements of spm

--- R/spm.R
+++ R/spm.R
@@ -13,7 +13,8 @@
 #' @export
 setGeneric(name = "spm",
            def = function(sspm_object,
-                          formula){
+                          formula,
+                          ...){
              standardGeneric("spm")
            }
 )
@@ -25,7 +26,7 @@ setGeneric(name = "spm",
 setMethod(f = "spm",
           signature(sspm_object = "sspm",
                     formula = "missing"),
-          function(sspm_object, formula){
+          function(sspm_object, formula, ...){
             cli::cli_alert_danger(" Argument 'formula' missing with no default")
           }
 )
@@ -35,6 +36,40 @@ setMethod(f = "spm",
 setMethod(f = "spm",
           signature(sspm_object = "sspm",
                     formula = "formula"),
-          function(sspm_object, formula){
+          function(sspm_object, formula, ...){
+
+            # 1. Is there a splitting scheme?
+            if(!is_split(sspm_object)){
+              stop("Data must be split with a test/train column.")
+            } else {
+              old_data <- spm_smoothed_data(sspm_object)
+              train_data <- old_data %>%
+                dplyr::filter(.data$train_test == TRUE)
+              spm_smoothed_data(sspm_object) <- train_data
+            }
+
+            # 1. call map_formula
+            data_frame <- spm_smoothed_data(sspm_object)
+            time_column <- spm_time_column(sspm_object)
+            boundaries <- spm_boundaries(sspm_object)
+
+            browser()
+
+            # Pass onto the sspm_data method
+            sspm_formula <- map_formula(data_frame = data_frame,
+                                        boundaries = boundaries,
+                                        formula = formula,
+                                        time_column = time_column,
+                                        ...)
+
+            spm_smoothed_data(sspm_object) <- old_data
+
+            # 2. call fit with ... arguments
+            # sspm_object_fitted <- sspm_object_joined %>%
+            #   fit_smooths(boundaries = boundaries,
+            #               keep_fit = keep_fit, predict = predict, ...)
+
+            return(sspm_object_fitted)
+
           }
 )
