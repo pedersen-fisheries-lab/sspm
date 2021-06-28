6582ee3cc54e0f30dfb0d0c50ba83f5620837cb6
Valentin <valentin.lucet@gmail.com>
2021-06-28 14:37:33 -0400

    updates

--- tests/testthat/helper-tests.R
+++ tests/testthat/helper-tests.R
@@ -14,54 +14,49 @@ borealis_points <- sspm:::borealis_points
 borealis_spatial <- sspm:::borealis_simulated_spatial
 predator_spatial <- sspm:::predator_simulated_spatial
 
-# Base objects
-sspm_dataset <- new("sspm_dataset",
-                 name = "Biomass",
-                 data = borealis_spatial,
-                 type = "biomass",
-                 time_column = "year_f",
-                 uniqueID = "uniqueID",
-                 coords = c('lon_dec','lat_dec'))
-
-sspm_dataset_pred <- new("sspm_dataset",
-                      name = "Predator",
-                      type = "predictor",
-                      data = predator_spatial,
-                      time_column = "year",
-                      uniqueID = "uniqueID",
-                      coords = c('lon_dec','lat_dec'))
-
-dataset_list <- list(sspm_dataset)
-names(dataset_list) <- sapply(dataset_list, spm_name)
-sspm_base <- new("sspm",
-                 name="Model test",
-                 datasets=dataset_list,
-                 boundaries=sfa_boundaries)
-
+# Method
 discret_method <- new("discretization_method",
                       name ="voronoi_method",
                       method = tesselate_voronoi)
 
-sspm_discrete <- new("sspm_discrete",
-                     name = spm_name(sspm_base),
-                     # NOTE: patch_id absent
-                     datasets = spm_datasets(sspm_base),
-                     boundaries = spm_boundaries(sspm_base),
-                     method = discret_method,
-                     patches = borealis_patches,
-                     points = borealis_points)
+# Boundaries
+boundary <- new("sspm_boundary",
+                boundaries = sfa_boundaries,
+                boundary_column = "sfa")
 
-dataset_list <- list(sspm_dataset, sspm_dataset_pred)
-names(dataset_list) <- sapply(dataset_list, spm_name)
-sspm_discrete_mapped <- new("sspm_discrete",
-                            name = spm_name(sspm_base),
-                            # NOTE: patch_id absent
-                            datasets = dataset_list,
-                            boundaries = spm_boundaries(sspm_base),
-                            method = discret_method,
-                            patches = borealis_patches,
-                            points = borealis_points)
+boundary_discrete <- new("sspm_discrete_boundary",
+                         boundaries = sfa_boundaries,
+                         boundary_column = "sfa",
+                         method = discret_method,
+                         patches = borealis_patches,
+                         points = borealis_points)
 
+# Base objects
+biomass_dataset <- new("sspm_dataset",
+                       name = "Biomass",
+                       data = borealis_spatial,
+                       type = "biomass",
+                       time_column = "year_f",
+                       uniqueID = "uniqueID",
+                       coords = c('lon_dec','lat_dec'))
+
+predator_dataset <- new("sspm_dataset",
+                        name = "Predator",
+                        type = "predictor",
+                        data = predator_spatial,
+                        time_column = "year",
+                        uniqueID = "uniqueID",
+                        coords = c('lon_dec','lat_dec'))
+
+catch_dataset <- new("sspm_dataset",
+                     name = "Catch",
+                     type = "catch",
+                     data = predator_spatial,
+                     time_column = "year",
+                     uniqueID = "uniqueID",
+                     coords = c('lon_dec','lat_dec'))
+
+# Formula
 sspm_formula <- new("sspm_formula",
                     raw_formula = as.formula("weight_per_km2 ~ smooth_time() +
                                                smooth_space() + smooth_space_time()"),
@@ -72,7 +67,51 @@ sspm_formula <- new("sspm_formula",
                       xt = list(year_f = list(penalty = pen_mat_time),
                       patch_id = list(penalty = pen_mat_space)))"),
                     vars = list(pen_mat_time = matrix(),
-                                pen_mat_space = matrix()))
+                                pen_mat_space = matrix()),
+                    response = "weight_per_km2")
 
-sspm_discrete_mapped_forms <- sspm_discrete_mapped
-sspm_discrete_mapped_forms@datasets$Biomass@formulas <- list(sspm_formula)
+# Smoothed objects
+biomass_dataset_smoothed <- new("sspm_dataset",
+                                name = "Biomass",
+                                data = borealis_spatial,
+                                type = "biomass",
+                                time_column = "year_f",
+                                uniqueID = "uniqueID",
+                                coords = c('lon_dec','lat_dec'),
+                                formulas = list(sspm_formula),
+                                smoothed_data = borealis_spatial,
+                                smoothed_fit = list(),
+                                is_mapped = TRUE)
+
+predator_dataset_smoothed <- new("sspm_dataset",
+                                 name = "Predator",
+                                 type = "predictor",
+                                 data = predator_spatial,
+                                 time_column = "year",
+                                 uniqueID = "uniqueID",
+                                 coords = c('lon_dec','lat_dec'),
+                                 formulas = list(sspm_formula),
+                                 smoothed_data = predator_spatial,
+                                 smoothed_fit = list(),
+                                 is_mapped = TRUE)
+# -------------------------------------------------------------------------
+
+all_data <- list(biomass = biomass_dataset_smoothed,
+                 predator = predator_dataset_smoothed,
+                 catch = catch_dataset)
+
+sspm_model <- new("sspm",
+                  datasets = all_data,
+                  time_column = spm_time_column(biomass_dataset_smoothed),
+                  uniqueID = "row_ID",
+                  boundaries = spm_boundaries(biomass_dataset_smoothed),
+                  smoothed_data = borealis_spatial,
+                  is_split = FALSE)
+
+sspm_fit <- new("sspm_fit",
+                smoothed_data = all_data,
+                time_column = spm_time_column(biomass_dataset_smoothed),
+                uniqueID = spm_unique_ID(biomass_dataset_smoothed),
+                formula = sspm_formula,
+                boundaries = spm_boundaries(biomass_dataset_smoothed),
+                fit = bam(data = mtcars, mpg~wt, family = gaussian))
