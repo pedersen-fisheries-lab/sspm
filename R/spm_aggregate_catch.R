a10b5b17529bf507f27fd44eb7e07e18e1f3d314
Valentin <valentin.lucet@gmail.com>
2021-07-13 11:01:44 -0400

    fix aggregate catch

--- R/spm_aggregate_catch.R
+++ R/spm_aggregate_catch.R
@@ -3,12 +3,12 @@
 #' Aggregate the catch data contained in a catch dataset and update the biomass
 #' dataset with the subtracted catch.
 #'
-#' @inheritParams spm_aggregate
 #' @param biomass **\[sspm_dataset (smoothed)\]** The dataset containing the
 #'     biomass variable.
 #' @param catch **\[sspm_dataset\]** The dataset containing the catch variable.
 #' @param biomass_variable **\[character\]** The biomass variab of `biomass`.
 #' @param catch_variable **\[character\]** The catch column of `catch`.
+#' @inheritParams spm_aggregate
 #'
 #' @return
 #' Updated `sspm_dataset`.
@@ -45,49 +45,80 @@ setMethod(f = "spm_aggregate_catch",
                    apply_to_df = FALSE,
                    ...){
 
+            if (!checkmate::test_subset(biomass_variable,
+                                        names(spm_smoothed_data(biomass)))) {
+              stop("`biomass_variable` must be a column of the smoothed data of `biomass`",
+                   call. = FALSE)
+            }
+
+            if (!checkmate::test_subset(catch_variable,
+                                        names(spm_data(catch)))) {
+              stop("`catch_variable` must be a column of `catch`",
+                   call. = FALSE)
+            }
+
             info_message <-
               paste0(" Offsetting biomass with catch data using columns: ",
                      paste(cli::col_green(c(biomass_variable, catch_variable)), collapse = ", "))
             cli::cli_alert_info(info_message)
 
+            # Get the right columns
+            biomass_time_col <- spm_time_column(biomass)
+            catch_time_col <- spm_time_column(catch)
+
+            # Aggregate the catch
             catch <- spm_aggregate(dataset = catch,
                                    boundaries = spm_boundaries(biomass),
                                    variable = catch_variable,
                                    fun = fun, group_by = group_by,
                                    fill = fill, apply_to_df = apply_to_df,
                                    na.rm = TRUE, ...)
-
-            # Calculate the right columns
-            smoothed_data_time_col <- spm_time_column(biomass)
-            time_col <- spm_time_column(catch)
+            catch_data <- spm_data(catch) %>%
+              dplyr::rename(!!biomass_time_col := catch_time_col)
 
             # First, join data
-            full_smoothed_data <- spm_data(biomass) %>%
-              dplyr::mutate(!!smoothed_data_time_col :=
-                              as.factor(.data[[smoothed_data_time_col]])) %>%
-              dplyr::rename(!!time_col := smoothed_data_time_col) %>%
-              dplyr::left_join(spm_data(catch),
-                               by = sapply(c(time_col, "patch_id"),
+            full_smoothed_data <- spm_smoothed_data(biomass) %>%
+              dplyr::mutate(!!biomass_time_col :=
+                              as.factor(.data[[biomass_time_col]])) %>%
+              dplyr::left_join(catch_data,
+                               by = sapply(c(biomass_time_col, "patch_id"),
                                            rlang::as_string))
 
-            catch_name <- paste0(biomass_variable, "_with_catch")
-            change_name <- paste0(biomass_variable, "_with_catch_change")
+            # Make correct names
+            catch_name <- paste(c(biomass_variable, spm_name(biomass),
+                                  "with_catch"), collapse = "_")
+            change_name <- paste0(c(biomass_variable, spm_name(biomass),
+                                    "with_catch_change"), collapse = "_")
 
+            # Calculate
             full_smoothed_data <- full_smoothed_data %>%
+
               dplyr::group_by(.data[["patch_id"]],
                               !!spm_boundary_colum(spm_boundaries(biomass))) %>%
+
+              dplyr::mutate(area_no_units = as.vector(.data$area)) %>%
               dplyr::mutate(
                 !!catch_name :=
-                  .data[[biomass_variable]] + .data / .data$area) %>%
+                  (.data[[biomass_variable]] + .data[[catch_variable]]) / .data$area_no_units) %>%
               dplyr::mutate(
                 !!change_name :=
                   log(.data[[catch_name]]) - log(dplyr::lag(.data[[biomass_variable]],
                                                             default = NA))) %>%
+
+              dplyr::select(-area_no_units) %>%
               dplyr::ungroup() %>%
+
               dplyr::mutate(!!change_name := ifelse(is.na(.data[[change_name]]),
                                                     0, .data[[change_name]])) %>%
+
               dplyr::relocate(dplyr::starts_with(biomass_variable),
-                              .after = .data$row_ID)
+                              .after = .data$row_ID) %>%
+              dplyr::mutate(!!biomass_time_col :=
+                              as.numeric(as.character(.data[[biomass_time_col]])))
+
+            spm_smoothed_data(biomass) <- full_smoothed_data
+
+            return(biomass)
 
           }
 )
