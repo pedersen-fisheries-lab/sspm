5429d5ac119d200c5ab8efe69e671d411aa33504
Valentin <valentin.lucet@gmail.com>
2021-07-13 00:47:52 -0400

    improve functionnality of spm_discretize

--- R/tesselate_voronoi.R
+++ R/tesselate_voronoi.R
@@ -8,12 +8,19 @@
 #' @param with **\[sf\]** A set of data points to use for voronoisation.
 #' @param boundary_column **\[character\]** The column in `boundaries` that is to
 #'     be used for the stratified sampling.
+#'
+#' @param sample_surface **\[logical]** Whether to sample the surfaces in
+#'     `boundaries`, Default to `FALSE`.
 #' @param sample_points **\[logical]** Whether to sample points from `with` or
 #'     to take all points in `with`. Default to `TRUE`.
+#'
 #' @param nb_samples **\[named character vector\]** The number of samples to draw
 #'     by boundary polygons (must bear the levels of `boundary_column` as names).
 #' @param min_size **\[numeric\]** The minimum size for a polygon above which it
 #'     will be merged (in km2).
+#' @param stratify **\[logical]** Whether the discretization happens within the
+#'     boundaries or whether the whole area is to be used (default to TRUE).
+#'
 #' @param seed **\[numeric\]** Passed onto [`set.seed()`][base::set.seed()],
 #'     important for reproducibility of sampling.
 #'
@@ -26,16 +33,15 @@
 tesselate_voronoi <- function(boundaries,
                               with,
                               boundary_column = "sfa",
+                              sample_surface = FALSE,
                               sample_points = TRUE,
-                              nb_samples = c(`4` = 10, `5` = 30,
-                                             `6` = 30, `7` = 5),
+                              nb_samples,
                               min_size = 1500,
+                              stratify = TRUE,
                               seed = 1) {
 
-  # TODO some steps in original code not supported as these are not general:
-  # To be discussed
-  # 1. breaking of big polygons
-  # 2. NAFO division
+  # TODO to be discussed
+  # 1. NAFO division
 
   # Prep --------------------------------------------------------------------
 
@@ -44,12 +50,6 @@ tesselate_voronoi <- function(boundaries,
     stop("boundaries argument is missing")
   } else {
     checkmate::assert_class(boundaries, "sf")
-  }
-
-  if (is.null(with)) {
-    stop("with argument is missing")
-  } else {
-    checkmate::assert_class(with, "sf")
   }
 
   checkmate::assert_logical(sample_points)
@@ -70,29 +70,77 @@ tesselate_voronoi <- function(boundaries,
   if (getRversion() >= 3.6) suppressWarnings(RNGkind(sample.kind = "Rounding"))
 
   # 2. Create (sample) the points
-  if (sample_points) {
+  # TODO revise here: allows stratified sampling, but doesn't allow a given
+  # number (like "want to sample 100" polygons) : check for length of nb_samples?
+
+  boundaries_split <- split(boundaries, boundaries[[boundary_column]])
+
+  if (sample_surface){
+
+    sample_fun <- function(polygon, boundary_column, nb_samples){
+      sf::st_sample(polygon,
+                    size = nb_samples[polygon[[boundary_column]]])
+    }
+
     set.seed(seed) ; voronoi_points <-
-      suppressMessages(sf::st_join(with, boundaries)) %>%
-      dplyr::filter(!is.na(eval(dplyr::sym(boundary_column)))) %>%
-      dplyr::group_by(.data[[boundary_column]]) %>%
-      # TODO revise here: allows stratified sampling, but doesn't allow a given
-      # number (like "want to sample 100" polygons)
-      dplyr::filter(1:dplyr::n() %in%
-                      sample(1:dplyr::n(),
-                             size = nb_samples[[.data[[boundary_column]][1]]]))
+      lapply(boundaries_split, FUN = sample_fun,
+             boundary_column = boundary_column,
+             nb_samples = nb_samples) %>%
+      lapply(sf::st_as_sf) %>%
+      dplyr::bind_rows() %>%
+      dplyr::rename(geometry = x) %>%
+      sf::st_join(boundaries)
+
   } else {
-    voronoi_points <- with
+
+    if (sample_points) {
+
+      set.seed(seed) ; voronoi_points <-
+        suppressMessages(sf::st_join(with, boundaries)) %>%
+        dplyr::filter(!is.na(eval(dplyr::sym(boundary_column)))) %>%
+        dplyr::group_by(.data[[boundary_column]]) %>%
+        dplyr::filter(1:dplyr::n() %in%
+                        sample(1:dplyr::n(),
+                               size = nb_samples[[.data[[boundary_column]][1]]]))
+    } else {
+
+      voronoi_points <- with
+
+    }
+
   }
 
   # 3. Create the polygons
-  voronoi <-
-    suppressAll(voronoi_points %>%
-                  sf::st_union() %>%
-                  sf::st_voronoi() %>%
-                  sf::st_cast() %>%
-                  sf::st_sf())
-  voronoi <-
-    suppressAll(sf::st_intersection(x = boundaries, y = voronoi))
+  if(stratify){
+
+    voronoi <- voronoi_points %>%
+      split(voronoi_points[[boundary_column]]) %>%
+      lapply(function(x) { suppressAll(sf::st_union(x)) } ) %>%
+      lapply(function(x) { suppressAll(sf::st_voronoi(x)) } ) %>%
+      lapply(function(x) { suppressAll(sf::st_cast(x)) } ) %>%
+      lapply(function(x) { suppressAll(sf::st_sf(x)) } ) %>%
+      mapply(FUN = function(x, y) { suppressAll(sf::st_intersection(x, y)) },
+             boundaries_split,
+             SIMPLIFY = FALSE) %>%
+      dplyr::bind_rows()
+
+    voronoi <- sf::st_sf(voronoi) %>%
+      dplyr::rename(geometry = x)
+
+  } else {
+
+    voronoi <-
+      suppressAll(voronoi_points %>%
+                    sf::st_union() %>%
+                    sf::st_voronoi() %>%
+                    sf::st_cast() %>%
+                    sf::st_sf())
+
+    voronoi <-
+      suppressAll(sf::st_intersection(x = boundaries, y = voronoi))
+
+  }
+
   voronoi <-
     suppressAll(voronoi %>%
                   dplyr::mutate(patch_id = paste("V", 1:dplyr::n(), sep = "")) %>%
@@ -107,7 +155,9 @@ tesselate_voronoi <- function(boundaries,
                               area = units::set_units(.data$area,
                                                       value = "km^2")))
 
-  # 4. Fix size
+  # 4. Merge small polygons
+  # TODO the removal of small polygons has not been stratified
+
   small_voronoi <- voronoi$patch_id[which(voronoi$area <
                                             units::set_units(min_size, value = "km^2"))]
   voronoi_edges <- suppressMessages(sf::st_intersects(voronoi))
@@ -116,11 +166,14 @@ tesselate_voronoi <- function(boundaries,
   # TODO vectorize this
   for (i in small_voronoi) {
     current_polygons <- voronoi[voronoi_edges[[i]], ] %>%
-      dplyr::filter(.data[[boundary_column]] == .data[[boundary_column]][.data$patch_id == i]) %>%
+      dplyr::filter(.data[[boundary_column]] ==
+                      unique(.data[[boundary_column]][.data$patch_id == i])) %>%
       dplyr::filter(.data$area == max(.data$area))
     max_id <- current_polygons$patch_id
     voronoi$patch_id[voronoi$patch_id == i] <- max_id
   }
+
+  # -------------------------------------------------------------------------
 
   # Summarise and calculate area
   voronoi <-
