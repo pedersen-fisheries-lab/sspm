3d77ac6a0ee6d94a3235073671d8fb14c1e0fe72
Valentin <valentin.lucet@gmail.com>
2021-10-21 16:12:04 -0400

    fix select function

--- R/predict.R
+++ R/predict.R
@@ -108,7 +108,7 @@ setMethod(f = "spm_predict_biomass",
             biomass_pred <- preds$pred * biomass
 
             biomass_pred_df <- data.frame(biomass_pred = biomass_pred) %>%
-              cbind(select(preds, -pred, -pred_log))
+              cbind(dplyr::select(preds, -pred, -pred_log))
 
             return(biomass_pred_df)
           }
