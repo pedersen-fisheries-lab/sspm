18184a1b26a35756deb0d623e825f1a7b25ef970
Valentin <valentin.lucet@gmail.com>
2022-01-24 22:04:11 -0500

    move helper

--- R/A_helpers.R
+++ R/A_helpers.R
@@ -38,30 +38,6 @@ spm_aggregation_choices <- function() {
   return(choices)
 }
 
-# Join datasets to patches
-join_datasets <- function(sspm_dataset, sspm_boundary) {
-
-  checkmate::assert_class(sspm_dataset, "sspm_dataset")
-  checkmate::assert_class(sspm_boundary, "sspm_discrete_boundary")
-
-  the_data <- spm_data(sspm_dataset)
-  the_patches <- sspm_boundary@patches
-
-  # TODO REVIEW THE COHERENCE OF ST_TRANSFORM
-  joined <- suppressMessages(sf::st_transform(the_data, crs = sf::st_crs(the_patches)))
-  # TODO joining patches to points but should be the opposite, keeping for rep for now
-  joined <- suppressMessages(sf::st_join(the_patches, the_data,
-                                         suffix	= c("", "_dup"))) %>%
-    dplyr::filter(!duplicated(.data[[spm_unique_ID(sspm_dataset)]])) %>%
-    dplyr::filter(!is.na(.data$patch_id))
-
-  spm_data(sspm_dataset) <- joined
-  spm_boundaries(sspm_dataset) <- sspm_boundary
-  is_mapped(sspm_dataset) <- TRUE
-
-  return(sspm_dataset)
-}
-
 # Suppress both messages and warnings
 suppressAll <- function(x) {
   suppressWarnings(suppressMessages(x))
