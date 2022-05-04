12f048ba764c975d3591d95fd6c4c52daefa8119
Valentin <valentin.lucet@gmail.com>
2022-05-04 11:48:40 -0400

    add data

---
+++ R/catch_simulated.R
@@ -0,0 +1,15 @@
+#' Simulated catch data
+#'
+#' Simulated catch data for test and practice.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{year_f}{Year as a factor}
+#'   \item{sfa}{SFA ID number}
+#'   \item{catch}{Simualated catch in kg}
+#'   \item{lon_dec}{Longitude}
+#'   \item{lat_dec}{Latitude}
+#'   \item{row}{Row ID}
+#'   \item{uniqueID}{Unique ID for simulated observation}
+#' }
+"catch_simulated"
