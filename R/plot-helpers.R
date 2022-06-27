ebc0ed4eac6929ee3d7e7ed8d7459b39786330eb
Valentin <valentin.lucet@gmail.com>
2022-06-27 11:40:50 -0400

    fix

--- R/plot-helpers.R
+++ R/plot-helpers.R
@@ -202,7 +202,6 @@ spm_plot_routine <- function(smoothed_data, var, use_sf, page, nrow, ncol,
     base_plot <- ggplot2::ggplot(data = smoothed_data) +
       ggplot2::geom_line(ggplot2::aes(x = .data[[time_col]],
                                       y = .data[[var]],
-                                      size = c(2),
                                       color = .data$color)) +
       ggplot2::geom_point(ggplot2::aes(x = .data[[time_col]],
                                        y = .data[[var]],
